81e621e168f76e3b4feedbde8d0f7b0400188fc2
p01terge1st <p01terge1st@yandex.ru>
2021-06-23 13:44:18 +0300

    file save function

---
+++ file_save.py
@@ -0,0 +1,13 @@
+#запись в файл:
+def file_save():
+    try:
+        with open (input('Название и путь файла куда сохранить:\n'), 'a') as file: #открываем куда писать полученные данные
+            file.write('blabla') # записываем файл. Вместо 'blabla' можно указать переменную 
+            print('Успешно сохранено.')
+    except FileNotFoundError:
+        print('Не могу найти указанный путь. Формат ввода: \'E:\passwords.txt\'')
+        file_save()
+    except PermissionError:
+        print('Доступ в указанную папку(каталог) запрещен!\n')
+        file_save()
+file_save()
